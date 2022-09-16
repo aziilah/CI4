64592c964aff2f5f0d081ede42900cc9a1682ff4
Aziilah <nuraziilahawg@gmail.com>
2022-09-17 00:12:48 +0800

    add layout template

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,10 +1,13 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
 <div class="container">
     <div class="row">
         <div class="col">
+
             <h1>About Me!</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit corporis ullam ex reprehenderit laborum quod magnam fugiat illum nisi praesentium!</p>
-
         </div>
     </div>
 </div>
-    
+<?= $this->endSection(); ?>
