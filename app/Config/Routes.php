607a79f8798f2b4c5ba34f3be97d6b89f7dd5005
Aziilah <nuraziilahawg@gmail.com>
2022-09-16 22:30:52 +0800

    Pages route

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,8 +35,8 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
-
+$routes->get('/', 'Pages::index');
+$routes->get('/pages/about', 'Pages::about');
 /*
  * --------------------------------------------------------------------
  * Additional Routing
