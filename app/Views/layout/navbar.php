4aa694d661d9c4ea70372f63f0cba8ed10536140
Aziilah <nuraziilahawg@gmail.com>
2022-09-17 16:13:48 +0800

    add comic menu

--- app/Views/layout/navbar.php
+++ app/Views/layout/navbar.php
@@ -15,6 +15,9 @@
         <li class="nav-item">
           <a class="nav-link" href="/pages/contact">Contact</a>
         </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/comics/index">Comic</a>
+        </li>
       </ul>
     </div>
   </div>
