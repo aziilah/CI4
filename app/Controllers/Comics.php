f92b3c2370350567bf4f20ea56af912574661f05
Aziilah <nuraziilahawg@gmail.com>
2022-09-18 01:30:10 +0800

    create comic and save data controller

--- app/Controllers/Comics.php
+++ app/Controllers/Comics.php
@@ -38,6 +38,45 @@ class Comics extends BaseController
            'title' => 'Comic Details',
            'comic' => $this->comicModel->getComic($slug)
         ];
+
+        //if comic not available in table
+        if(empty($data['comic'])) {
+            throw new \CodeIgniter\Exceptions\PageNotFoundException('Comic Title ' . $slug . ' is not found!');
+        }
         return view ('comics/detail', $data);
+    }
+
+    public function create() {
+        $data = [
+            'title' => 'Data Comic Form',
+            // 'comic' => $this->comicModel->getComic()
+        ];
+
+        return view ('comics/create', $data);
+    }
+
+    //handle posted data from create to insert into table
+    public function save() {
+        // dd($this->request->getVar('title'));
+
+        $slug = url_title($this->request->getVar('title'), '-', true);
+        //insert to db
+        $this->comicModel->save([
+            'title' => $this->request->getVar('title'),
+            'slug' => $slug,
+            'author' => $this->request->getVar('author'),
+            'publisher' => $this->request->getVar('publisher'),
+            'image' => $this->request->getVar('image')
+
+        ]);
+
+        //make flash data in index.php
+        session()->setFlashdata('warning', 'Data has been added!');
+
+        //after save, redirect to index
+        return redirect()->to('/comics/index');
+
+        // return view ('comics/save');
+        
     }
 }
