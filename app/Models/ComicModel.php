f1c7691052308a37a757bece3aad08bf7154f5ec
Aziilah <nuraziilahawg@gmail.com>
2022-09-17 18:05:24 +0800

    getComic function slug

--- app/Models/ComicModel.php
+++ app/Models/ComicModel.php
@@ -9,4 +9,14 @@ class ComicModel extends Model
     protected $table = 'comic'; //table name in db
     // protected $primaryKey = 'id'; //default
     protected $useTimestamps = true;
+
+    public function getComic($slug = false)
+    {
+        if ($slug == false) {
+            return $this->findAll();
+        }
+
+        return $this->where(['slug' => $slug]) -> first();
+    }
+
 }
