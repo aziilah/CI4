6fa86c7f295eb2cf43c18e450c827ad4a560ea90
Aziilah <nuraziilahawg@gmail.com>
2022-09-19 00:14:35 +0800

    query for searching keyword

--- app/Models/AuthorModel.php
+++ app/Models/AuthorModel.php
@@ -9,4 +9,13 @@ class AuthorModel extends Model
     protected $table = 'author';
     protected $useTimestamps = true;
     protected $allowedFields = ['name', 'address'];
+
+    public function search($keyword)
+    {
+        // $builder = $this->table('author');
+        // $builder->like('name', $keyword);
+        // return $builder;
+
+        return $this->table('author')->like('name', $keyword)->orLike('address', $keyword);
+    }
 }
