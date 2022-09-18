982a15eb882e35bd3efc1f21ccaac599761a31cf
Aziilah <nuraziilahawg@gmail.com>
2022-09-18 22:08:15 +0800

    authors view

---
+++ app/Views/authors/index.php
@@ -0,0 +1,37 @@
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+
+            <h1 class="mt-2">Author Registration</h1>
+
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Address</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php $i = 1; ?>
+                    <?php foreach ($author as $a) : ?>
+                        <tr>
+                            <th scope="row"><?= $i++; ?></th>
+                            <td><?= $a['name']; ?></td>
+                            <td><?= $a['address']; ?></td>
+                            <td>
+                                <a href="" class="btn btn-success">Details</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+<?= $this->endSection(); ?>
