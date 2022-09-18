6aa46d42f0ddcc0f17747ba4a40ae4c4ff6d22a3
Aziilah <nuraziilahawg@gmail.com>
2022-09-18 17:10:27 +0800

    image preview dom

--- app/Views/layout/template.php
+++ app/Views/layout/template.php
@@ -23,5 +23,24 @@
 <!-- footer -->
     <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    <script>
+      function previewImg() {
+        const image = document.querySelector('#image')
+        const imageLabel = document.querySelector('.custom-file-label');
+        const imgPreview = document.querySelector('.img-preview');
+  
+        //label content
+        imageLabel.textContent = image.files[0].name;
+  
+        //change image preview
+        const fileImage = new FileReader();
+        fileImage.readAsDataURL(image.files[0]);
+  
+        fileImage.onload = function(e) {
+          imgPreview.src = e.target.result;
+        }
+      } 
+
+    </script>
   </body>
 </html>
