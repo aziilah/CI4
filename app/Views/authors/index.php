8f52f4fb059e26b962b92cc9ca06732d774c9ae5
Aziilah <nuraziilahawg@gmail.com>
2022-09-19 00:15:38 +0800

    display searching author

--- app/Views/authors/index.php
+++ app/Views/authors/index.php
@@ -3,9 +3,21 @@
 <?= $this->section('content'); ?>
 <div class="container">
     <div class="row">
+        <div class="col-6">
+            <h1 class="mt-2">Author Registration</h1>
+            <form action="" method="post">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Enter author keywords..." name="keyword">
+                    <div class="input-group-append">
+                        <button class="btn btn-outline-secondary" type="submit" id="submit">Search</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <div class="row">
         <div class="col">
 
-            <h1 class="mt-2">Author Registration</h1>
 
             <table class="table">
                 <thead>
