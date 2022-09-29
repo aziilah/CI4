adffeafc06693a4f8e931a3678ca65ce03fe10e9
Aziilah <nuraziilahawg@gmail.com>
2022-09-29 20:58:49 +0800

    sending emails for pwd reset

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
     /**
      * @var string
      */
-    public $fromEmail;
+    public $fromEmail = 'ilahawg@gmail.com';
 
     /**
      * @var string
      */
-    public $fromName;
+    public $fromName = 'IlahAwang';
 
     /**
      * @var string
